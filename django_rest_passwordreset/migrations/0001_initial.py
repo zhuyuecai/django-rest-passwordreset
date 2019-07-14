68964e1803341ba9a04f86bf47b14cff4f2c3fa6
Victor <zhuyuecai@gmail.com>
2019-07-14 17:16:04 -0400

    Update 0001_initial.py

--- django_rest_passwordreset/migrations/0001_initial.py
+++ django_rest_passwordreset/migrations/0001_initial.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
             name='ResetPasswordToken',
             fields=[
                 ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='When was this token generated')),
-                ('key', models.CharField(max_length=64, primary_key=True, serialize=False, verbose_name='Key')),
+                ('key', models.CharField(max_length=64, serialize=False, verbose_name='Key')),
                 ('ip_address', models.GenericIPAddressField(default='127.0.0.1', verbose_name='The IP address of this session')),
                 ('user_agent', models.CharField(default='', max_length=256, verbose_name='HTTP User Agent')),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='password_reset_tokens', to=settings.AUTH_USER_MODEL, verbose_name='The User which is associated to this password reset token')),
